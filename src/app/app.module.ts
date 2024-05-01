05f25c61658101d1219084d8984e28251e202344
luc4sdreyer <luc4sdreyer@gmail.com>
2024-05-01 11:47:01 +0200

    changed to standalone module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,8 +9,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    CommonModule
+    BrowserModule,
+    AppRoutingModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
